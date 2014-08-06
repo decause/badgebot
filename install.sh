1fdb9f5057c6543598c59427dd5a56ad39a51e84
Ralph Bean <rbean@redhat.com>
2014-08-06 09:30:45 +0200

    Remove threebean-ism.

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -x
+# install.sh - (re)install and (re)start the badgebot
 
+# Install our script
 cp badgebot.py /usr/local/bin/badgebot.py
-cp /home/threebean/devel/badgebot/badgebot.service /usr/lib/systemd/system/badgebot.service
-cp fedmsg.d/*.py /etc/fedmsg.d/.
+
+# Make sure no one else can read our secrets.
+cp fedmsg.d/twitter-scripts.py /etc/fedmsg.d/.
+chown fedmsg:fedmsg /etc/fedmsg.d/twitter-secrets.py
+chmod o-r /etc/fedmsg.d/twitter-secrets.py
+
+# Copy in service file for systemd
+cp badgebot.service /usr/lib/systemd/system/badgebot.service
 systemctl daemon-reload
 systemctl restart badgebot
